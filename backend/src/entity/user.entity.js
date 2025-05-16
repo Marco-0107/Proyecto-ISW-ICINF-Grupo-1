b35a6fc1b393a4ba3715c9626e399da4b013fd81
thecris_t <cristian.torres2101@alumnos.ubiobio.cl>
2025-05-16 01:37:01 -0400

    Actualización
    Se creo la entidad Publicacion

--- backend/src/entity/user.entity.js
+++ backend/src/entity/user.entity.js
@@ -80,42 +80,5 @@ const Usuario = new EntitySchema({
   ],
 });
 
-const Publicacion = new EntitySchema({
-  name: "Publicacion",
-  tableName: "publicacion",
-  columns:{
-    id_publicacion: {
-      primary: true,
-      type: "int",
-      generated: true
-    },
-    titulo:{
-      type: "varchar",
-      length: 83,
-      nullable: false
-    },
-    tipo:{
-      type: "varchar",
-      length: 32,
-      nullable: false
-    },
-    contenido:{
-      type: "varchar",
-      length: 1024,
-      nullable: false
-    },
-    fecha_publicacion:{
-      type: "date",
-      nullable: false
-    },
-  },
-  relations:{
-    usuario:{
-      type:"many-to-one",
-      target: "Usuario",
-      joinColumn:{name: "id_usuario"},
-    },
-  },
-})
 
-export default (Usuario, Publicacion);
+export default (Usuario);
