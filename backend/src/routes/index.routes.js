35bed3f518c2e79fad533219e37753f1dc8bd640
Marco Cerda Chamorro <marco.cerda2101@alumnos.ubiobio.cl>
2025-07-14 02:19:39 -0400

    ADD de funcionalidad (NodeMailer)
    Se implemento el envio automatizado de correos electronicos en el backend, utilizando nodemailer.

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -15,12 +15,14 @@ import usuarioReunionRoutes from "./usuario_reunion.routes.js";
 import usuarioCuotaRoutes from "./usuario_cuota.routes.js";
 import usuarioConvocatoriaRoutes from "./usuario_convocatoria.routes.js";
 import archivoRoutes from "./archivo.routes.js"
+import emailRoutes from "./email.routes.js";
 
 const router = Router();
 
 router
     .use("/auth", authRoutes)
     .use ("/archivo", archivoRoutes)
+    .use("/email", emailRoutes)
     .use("/user", userRoutes)
     .use("/publicacion", publicacionRoutes)
     .use("/reunion", reunionRoutes)
